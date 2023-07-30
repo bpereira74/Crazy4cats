f9f7668f666b26c2bc9ee65383c2de3180759953
bpereira74 <bpereira74@hotmail.com>
2023-07-30 02:23:51 -0400

    feat: Nueva publicacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  resources :publications
+  
   devise_for :users
-  get 'home/index'
+      root 'home#index'
+  
+
+  authenticated :user do
+      root 'post#index', as: :authenticated_root
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "home#index"
+  
+  
 end
